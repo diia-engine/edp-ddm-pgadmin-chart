acfa4894cfd649a32d1a7220e7bb8373b33dadcc
jenkins <jenkins@epam.com>
2024-10-17 11:11:26 +0300

    [EDP-DDM-0000] Bulk update 1.9.8-hotfix

--- upgrade-scripts/pre-upgrade/one-time/ClearDepricatedSessions.groovy
+++ upgrade-scripts/pre-upgrade/one-time/ClearDepricatedSessions.groovy
@@ -1,11 +1,28 @@
 void call() {
+    String dep = ''
+    String replicas = ''
+    String[] deployments = ['pgadmin-deployment', 'pgadmin-chart-deployment']
 
-    String replicas = sh(script: "oc get deployment pgadmin-deployment -o jsonpath='{.spec.replicas}' -n $NAMESPACE", returnStdout: true).trim()
+    // Try each deployment until one is found
+    for (d in deployments) {
+        replicas = sh(
+                script: "oc get deployment ${d} -o jsonpath='{.spec.replicas}' -n $NAMESPACE || true",
+                returnStdout: true
+        ).trim()
+        if (replicas) {
+            dep = d
+            break
+        }
+    }
 
-    // Reload pgAdmin for remove deprecated sessions
-    sh "oc scale deployment/pgadmin-deployment --replicas=0 -n $NAMESPACE || true"
-    sh "oc scale deployment/pgadmin-deployment --replicas=${replicas} -n $NAMESPACE || true"
+    if (!dep) {
+        echo "No pgadmin deployment found in namespace $NAMESPACE"
+        return
+    }
 
+    // Reload pgAdmin to remove deprecated sessions
+    sh "oc scale deployment/${dep} --replicas=0 -n $NAMESPACE || true"
+    sh "oc scale deployment/${dep} --replicas=${replicas} -n $NAMESPACE || true"
 }
 
-return this;
+return this
